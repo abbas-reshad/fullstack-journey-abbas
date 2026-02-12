3f6831b98937fee299b9f7c5cfb6a2cb921b5cff
Ssinaan <ssinaan.abbas@gmail.com>
2026-02-13 01:51:51 +0300

    feat: add User type and basic parseUser validation

---
+++ project-2-data-parser/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const user = {
+    id: 1,
+    name: "Abbas",
+    email: "abbas@mail.com"
+};
+console.log("User:", user);
+//# sourceMappingURL=index.js.map
---
+++ project-2-data-parser/src/index.ts
@@ -0,0 +1,13 @@
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const user: User = {
+  id: 1,
+  name: "Abbas",
+  email: "abbas@mail.com"
+};
+
+console.log("User:", user);
